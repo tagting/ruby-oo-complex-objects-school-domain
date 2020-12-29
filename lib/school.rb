7da9de46d5e70ed123a778ebb6e15206506789da
ting <ttzhenng@gmail.com>
2020-12-28 19:14:01 -0500

    commit tree 9ed643fa10b5f9305dbc6ea2d81f7487ee546b4e

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+    attr_accessor :name, :roster
+
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        roster[grade] ||= [] #roster[grade] ||= [] #?
+        roster[grade] << student #roster[grade] << student
+    end
+         
+
+    def grade(student_grade)
+        roster[student_grade]
+    end
+    
+    def sort
+        newlist = {}
+        roster.each do |grade, students|
+            newlist[grade] = students.sort
+        end
+        newlist
+    end
+
+
+end
